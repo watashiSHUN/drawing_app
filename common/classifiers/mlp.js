41e5420c6b229c96f866a034650abf6ff0829c96
Shun Gram <mike53669@gmail.com>
2025-02-17 17:49:00 -0800

    [DEBUG] working prototype of nerual network

--- common/classifiers/mlp.js
+++ common/classifiers/mlp.js
@@ -11,10 +11,10 @@ class NeuralNetwork {
       throw new Error("At least 2 levels are required");
     }
     this.levels = [];
-    // Each level needs its own size and its output size
+    // Each level needs its own input size and its output size
     // Create 1 level for each input
     for (let i = 0; i < neurons_per_level.length - 1; i++) {
-      let level = new Level(neurons_per_level[i], neurons_per_level[i + 1]);
+      let level = new Level(neurons_per_level[i], neurons_per_level[i + 1], i);
       level.randomInitialize();
       this.levels.push(level);
     }
@@ -26,21 +26,24 @@ class NeuralNetwork {
     }
     // link between the levels
     let outputs = inputs;
-    for (let i = 0; i < this.levels.length - 1; i++) {
+    // NOTE, we need to iterate through all levels
+    for (let i = 0; i < this.levels.length; i++) {
+      console.log(`Level [${i}]->[${i + 1}] input: ${outputs}`);
       outputs = this.levels[i].feedForward(outputs);
     }
+    console.log(`Final output: ${outputs}`);
     return outputs;
   }
 }
 
 class Level {
-  constructor(input_size, output_size) {
-    // TODO, what's the input range?
+  constructor(input_size, output_size, row) {
     this.input_nodes = new Array(input_size);
     for (let i = 0; i < this.input_nodes.length; i++) {
-      this.input_nodes[i] = new Node(output_size);
-      this.input_nodes[i].random_initialize();
+      this.input_nodes[i] = new Node(output_size, row, i);
+      this.input_nodes[i].randomInitialize();
     }
+    this.row = row;
     // output are values [0,1] one or zeros
     // Output threshold, a value above which the output is considered as 1
     this.output_biases = new Array(output_size);
@@ -55,12 +58,13 @@ class Level {
   }
   // [0.1, 0.2, 0.3, 0.4]
   feedForward(inputs) {
-    let output_values = new Array(this.output_biases.length);
+    let output_values = new Array(this.output_biases.length).fill(0);
     for (let i = 0; i < inputs.length; i++) {
       let outputs_from_single_input = this.input_nodes[i].feedForward(
         inputs[i]
       );
       for (let j = 0; j < outputs_from_single_input.length; j++) {
+        // NOTE: we cannot add numbers to empty
         output_values[j] += outputs_from_single_input[j];
       }
     }
@@ -71,7 +75,9 @@ class Level {
 }
 
 class Node {
-  constructor(neighbors_count) {
+  constructor(neighbors_count, row, column) {
+    this.row = row;
+    this.column = column;
     this.neighbors = new Array(neighbors_count);
   }
   // Initialize the edges with random values
@@ -79,8 +85,19 @@ class Node {
     for (let i = 0; i < this.neighbors.length; i++) {
       this.neighbors[i] = Math.random();
     }
+    console.log(
+      `node[${this.row}][${this.column}] edge values: ${this.neighbors}`
+    );
   }
   feedForward(input) {
     return this.neighbors.map((x) => x * input);
   }
+}
+
+// Unit test
+if (require.main === module) {
+  // if the current module is the main entry point of the program.
+  let nn = new NeuralNetwork([3, 4, 2]);
+  nn.feedForward([0, 0, 0]); // expect output [0,0]
+  nn.feedForward([0.1, 0.22, 0.333]);
 }
